fa4fa03252b31cb71f04b90eccb75210fc5dbf7a
dirtyCount <dirtyCount@gmail.com>
2020-11-11 11:31:46 +0000

    added colour codes for employees off site absent

--- libs/js/company-departments.js
+++ libs/js/company-departments.js
@@ -1,7 +1,7 @@
 import { getAllTables, getEmployee, updateEmployee, employeeSaveUpdate} from './ajax-calls.js';
 import { displayEmployeeInfoModal } from './display-functions.js';
 
-var employees, departments, locations;
+var employees, departments, locations, statuses;
 
 $( document ).ready(function() {
 
@@ -17,6 +17,8 @@ function displayDepartmentPageData(tablesInput) {
   departments = tablesInput['departments'];
   console.log(departments)
   employees = tablesInput['employees'];
+  statuses = tablesInput['status'];
+  console.log(statuses)
 
   // Adds locations then departments in that location then employees in that department
   departments.forEach(function(department) {
@@ -30,16 +32,29 @@ function displayDepartmentPageData(tablesInput) {
     } else {
       managerName = department['managerFirstName'] + ' ' + department['managerLastName'];
     }
-    $('#' + departmentIdTag).append('<h5>Manager: ' + managerName + '</h5><h5>' + department['location'] + '</h5></div>');
+    $('#' + departmentIdTag).append('<h5>Manager: ' + managerName + '</h5>');
+
+    $('#' + departmentIdTag).append('<div id="' + departmentIdTag + 'LastLine" class="department-lastline">');
+    $('#' + departmentIdTag + 'LastLine').append('<h5>' + department['location'] + '</h5></div>');
 
     // Button to toggle dropdown
-    $('#' + departmentIdTag).append('<button data-toggle="collapse" data-target="#' + departmentEmployeesIdTag + '" class="btn btn-info department-show-employees">Show Employees</button>');
-    // Creates dropdown for containing employees
     var departmentEmployeesIdTag = 'department-employees' + department['id'];
+    $('#' + departmentIdTag + 'LastLine').append('<button data-toggle="collapse" data-target="#' + departmentEmployeesIdTag + '" class="btn btn-info department-show-employees">Show Employees</button>');
+
+    // Creates dropdown for containing employees
     $('#' + departmentIdTag).append('<div id="' + departmentEmployeesIdTag + '" class="department-employees collapse">');
     employees.forEach(function(employee) {
-      if (employee['departmentID'] === department['id']) {
+      if (employee['departmentID'] === department['id'] && employee['id'] != department['departmentManager']) {
         $('#' + departmentEmployeesIdTag).append( '<div id="employee' + employee['id'] + '" class="employee-name btn btn-outline-dark" data-id=' + employee['id'] + '>' + employee['firstName'] + ' ' + employee['lastName'] + '</div>');
+        if (employee['currentLocationId'] !== department['locationID']) {
+          $('#employee' + employee['id']).addClass('offsite-employee');
+        }
+        if (employee['currentLocationId'] !== department['locationID']) {
+          $('#employee' + employee['id']).addClass('offsite-employee');
+        }
+        if (employee['status'] != 1) {
+          $('#employee' + employee['id']).addClass('absent-employee');
+        }
       }
     });
   })
@@ -60,7 +75,7 @@ $(document).on('click', '.branchManager', function () {
 
 $(document).on('click', '#employeeUpdateButton', function () {
   var employeeId = $(this).data("id");
-  updateEmployee(employeeId, departments, locations);
+  updateEmployee(employeeId, departments, locations, statuses);
 });
 
 $(document).on('submit', '#updateEmployeeModalForm', function () {
--- libs/php/getAllTables.php
+++ libs/php/getAllTables.php
@@ -125,6 +125,35 @@
 
 	$output['data']['departments'] = $data;
 
+	$query = 'SELECT * FROM status';
+
+	$result = $conn->query($query);
+
+	if (!$result) {
+
+		$output['status']['code'] = "400";
+		$output['status']['name'] = "executed";
+		$output['status']['description'] = "query failed";
+		$output['data'] = [];
+
+		mysqli_close($conn);
+
+		echo json_encode($output);
+
+		exit;
+
+	}
+
+   	$data = [];
+
+	while ($row = mysqli_fetch_assoc($result)) {
+
+		array_push($data, $row);
+
+	}
+
+	$output['data']['status'] = $data;
+
 	$output['status']['code'] = "200";
 	$output['status']['name'] = "ok";
 	$output['status']['description'] = "success";
